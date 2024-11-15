da711383488dd5e4454a16eb5838cd5e49ce9e49
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-15 13:24:38 +0100

    feat: introduce solution component

---
+++ components/Container/Solution/ContainerSolution.vue
@@ -0,0 +1,25 @@
+<template>
+	<UIContainer class="container-solution">
+		<template #heading>
+			<UIContainerHeading>
+				<template #icon>
+					<IconChat />
+				</template>
+				<template #title>
+					<h2>Votre solution sécurisée, sur mesure, avec des aides à portée de main</h2>
+				</template>
+			</UIContainerHeading>
+			<UIContainerContent>
+				<template #content>
+					<ContainerSolutionContent />
+				</template>
+			</UIContainerContent>
+		</template>
+	</UIContainer>
+</template>
+
+<style scoped lang="scss">
+.container-solution {
+	background-color: var(--extra-light-green);
+}
+</style>
---
+++ components/Container/Solution/ContainerSolutionContent.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="container-solution-content">
+		<UIContainerContentCard>
+			<template #icon>
+				<IconLaugh />
+			</template>
+			<template #title>
+				<h3>
+					Accompagnement personnalisé
+				</h3>
+			</template>
+			<template #text>
+				<p>
+					Nous vous accompagnons à chaque étape de votre parcours de soins.<br>Nos psychologues sont là pour vous écouter, vous guider, et vous aider à retrouver un équilibre. Chaque session est adaptée à vos besoins spécifiques.
+				</p>
+			</template>
+		</UIContainerContentCard>
+		<UIContainerContentCard>
+			<template #icon>
+				<IconLaugh />
+			</template>
+			<template #title>
+				<h3>
+					Accompagnement personnalisé
+				</h3>
+			</template>
+			<template #text>
+				<p>
+					Nous vous accompagnons à chaque étape de votre parcours de soins.<br>Nos psychologues sont là pour vous écouter, vous guider, et vous aider à retrouver un équilibre. Chaque session est adaptée à vos besoins spécifiques.
+				</p>
+			</template>
+		</UIContainerContentCard>
+		<UIContainerContentCard>
+			<template #icon>
+				<IconLaugh />
+			</template>
+			<template #title>
+				<h3>
+					Accompagnement personnalisé
+				</h3>
+			</template>
+			<template #text>
+				<p>
+					Nous vous accompagnons à chaque étape de votre parcours de soins.<br>Nos psychologues sont là pour vous écouter, vous guider, et vous aider à retrouver un équilibre. Chaque session est adaptée à vos besoins spécifiques.
+				</p>
+			</template>
+		</UIContainerContentCard>
+		<UIContainerContentCard>
+			<template #icon>
+				<IconLaugh />
+			</template>
+			<template #title>
+				<h3>
+					Mutuelles et complémentaires santé
+				</h3>
+			</template>
+			<template #text>
+				<p>
+					les psychologues conventionnés seront en général facilement remboursés par une mutuelle.<br>Les séances avec un psychothérapeute ou un psychanalyste peuvent aussi être remboursées par certaines mutuelles, souvent dans le cadre de packs dédiés aux "médecines douces" ou "thérapies alternatives".
+				</p>
+			</template>
+		</UIContainerContentCard>
+		<UIContainerContentCard>
+			<template #icon>
+				<IconLaugh />
+			</template>
+			<template #title>
+				<h3>
+					Mutuelles et complémentaires santé
+				</h3>
+			</template>
+			<template #text>
+				<p>
+					les psychologues conventionnés seront en général facilement remboursés par une mutuelle.<br>Les séances avec un psychothérapeute ou un psychanalyste peuvent aussi être remboursées par certaines mutuelles, souvent dans le cadre de packs dédiés aux "médecines douces" ou "thérapies alternatives".
+				</p>
+			</template>
+		</UIContainerContentCard>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.container-solution-content {
+	display: flex;
+	flex-wrap: wrap;
+
+	@include gap(24);
+	@include prop("padding-inline", 60);
+
+	& > div {
+		flex: 1;
+		@include prop("min-width", 315);
+	}
+}
+</style>
