8cbe91bbc1892e4a20b773e040ce044d129693ff
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-15 15:36:00 +0100

    feat: introduce solution component

---
+++ components/Container/Team/ContainerTeam.vue
@@ -0,0 +1,40 @@
+<template>
+	<UIContainer class="container-team">
+		<template #heading>
+			<UIContainerHeading>
+				<template #icon>
+					<IconChat />
+				</template>
+				<template #title>
+					<h2>
+						Notre équipe de professionnels
+					</h2>
+				</template>
+				<template #text>
+					<p>
+						Chez MyPsy, nous réunissons des experts pour vous offrir un accompagnement personnalisé et de qualité, adapté à vos besoins.<br>Notre équipe vous assure un soutien professionnel en psychanalyse, psychothérapie et psychopraticie, avec une approche moderne et bienveillante.
+					</p>
+				</template>
+				<template #cta>
+					<UIButton class="container-team__cta">
+						Découvrir <IconArrowTopLeft />
+					</UIButton>
+				</template>
+			</UIContainerHeading>
+			<UIContainerContent>
+				<template #content>
+					<ContainerTeamContent />
+				</template>
+			</UIContainerContent>
+		</template>
+	</UIContainer>
+</template>
+
+<style scoped lang="scss">
+.container-team {
+	&__cta {
+		color: var(--white);
+		background-color: var(--green);
+	}
+}
+</style>
---
+++ components/Container/Team/ContainerTeamContent.vue
@@ -0,0 +1,102 @@
+<template>
+	<div class="container-team-content">
+		<img src="/assets/images/vector-1.svg" class="container-team-content__background">
+
+		<ContainerTeamContentCard>
+			<template #avatar>
+				<img src="/assets/images/avatar.png" alt="">
+			</template>
+			<template #name>
+				<h3>
+					Nathalie Bourgeois
+				</h3>
+			</template>
+			<template #job>
+				<p>
+					Psychanalyste et Psychothérapeuthe
+				</p>
+			</template>
+			<template #bio>
+				<p class="container-team-content__bio">
+					Directrice de l'Institut de psychanalyse de Lyon Grenoble Annecy - fédaration de psychanalyse contemporaine
+				</p>
+			</template>
+			<template #link>
+				en savoir plus
+			</template>
+		</ContainerTeamContentCard>
+		<ContainerTeamContentCard>
+			<template #avatar>
+				<img src="/assets/images/avatar.png" alt="">
+			</template>
+			<template #name>
+				<h3>
+					Nathalie Bourgeois
+				</h3>
+			</template>
+			<template #job>
+				<p>
+					Psychanalyste et Psychothérapeuthe
+				</p>
+			</template>
+			<template #bio>
+				<p class="container-team-content__bio">
+					Directrice de l'Institut de psychanalyse de Lyon Grenoble Annecy - fédaration de psychanalyse contemporaine
+				</p>
+			</template>
+			<template #link>
+				en savoir plus
+			</template>
+		</ContainerTeamContentCard>
+		<ContainerTeamContentCard>
+			<template #avatar>
+				<img src="/assets/images/avatar.png" alt="">
+			</template>
+			<template #name>
+				<h3>
+					Nathalie Bourgeois
+				</h3>
+			</template>
+			<template #job>
+				<p>
+					Psychanalyste et Psychothérapeuthe
+				</p>
+			</template>
+			<template #bio>
+				<p class="container-team-content__bio">
+					Directrice de l'Institut de psychanalyse de Lyon Grenoble Annecy - fédaration de psychanalyse contemporaine
+				</p>
+			</template>
+			<template #link>
+				en savoir plus
+			</template>
+		</ContainerTeamContentCard>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.container-team-content {
+	position: relative;
+	display: flex;
+	justify-content: center;
+
+	@include prop("gap", 24);
+
+	&__background {
+		width: 100vw;
+		max-width: unset;
+
+		position: absolute;
+		top: -25%;
+	}
+
+	& > div {
+		flex: 1;
+		@include prop("max-width", 306);
+	}
+
+	&__bio {
+		@include font("fs-14-500");
+	}
+}
+</style>
