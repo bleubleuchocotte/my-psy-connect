8a2a020c6244f8b0a179e3b1add275e25e503ef5
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-15 18:39:47 +0100

    feat: introduce component

---
+++ components/Container/Advantages/Solo/ContainerAdvantagesSoloContent.vue
@@ -0,0 +1,67 @@
+<template>
+	<UIContainerContent class="container-advantages-solo-content">
+		<template #content>
+			<UITable class="container-advantages-solo-content__table">
+				<UITableItem>
+					<template #icon>
+						<IconMan />
+					</template>
+					<template #text>
+						<p>Devenez acteur de votre santé mentale avec un plan personnalisé</p>
+					</template>
+				</UITableItem>
+				<UITableItem>
+					<template #icon>
+						<IconFormation />
+					</template>
+					<template #text>
+						<p>Bénéficiez de l'expertise de professionnels sensibilisés à la souffrance au travail</p>
+					</template>
+				</UITableItem>
+				<UITableItem>
+					<template #icon>
+						<IconSolutions />
+					</template>
+					<template #text>
+						<p>Recevez des solutions concrètes adaptées aux réalités du monde professionnel</p>
+					</template>
+				</UITableItem>
+				<UITableItem>
+					<template #icon>
+						<IconCalendar />
+					</template>
+					<template #text>
+						<p>Accédez à un suivi rapide et flexible avec des rendez-vous personnalisés</p>
+					</template>
+				</UITableItem>
+				<UITableItem>
+					<template #icon>
+						<IconTV />
+					</template>
+					<template #text>
+						<p>Choisissez facilement le professionnel idéal grâce aux vidéos de présentation</p>
+					</template>
+				</UITableItem>
+				<UITableItem>
+					<template #icon>
+						<IconLeaf />
+					</template>
+					<template #text>
+						<p>Profitez d'un accompagnement ciblé, diminuant votre stress et favorisant un bien-être durable</p>
+					</template>
+				</UITableItem>
+			</UITable>
+		</template>
+	</UIContainerContent>
+</template>
+
+<style scoped lang="scss">
+.container-advantages-solo-content {
+	width: 100%;
+
+	&__table {
+		color: var(--dark-blue);
+		width: 100%;
+	}
+}
+</style>
---
+++ components/Container/Advantages/Solo/ContainerAdvantagesSoloHeading.vue
@@ -0,0 +1,42 @@
+<template>
+	<UIContainerHeading class="container-advantages-solo-heading">
+		<template #icon>
+			<IconChat class="container-advantages-solo-heading__icon" />
+		</template>
+		<template #title>
+			<h2 class="container-advantages-solo-heading__title">
+				Avantages des séances individuelles MyPsy
+			</h2>
+		</template>
+		<template #text>
+			<p class="container-advantages-solo-heading__text">
+				Chez MyPsy, nous réunissons des experts pour vous offrir un accompagnement personnalisé et de qualité, adapté à vos besoins.<br>Notre équipe vous assure un soutien professionnel en psychanalyse, psychothérapie et psychopraticie, avec une approche moderne et bienveillante.
+			</p>
+		</template>
+		<template #cta>
+			<UIButton class="container-advantages-solo-heading__cta">
+				En savoir plus <IconArrowTopLeft />
+			</UIButton>
+		</template>
+	</UIContainerHeading>
+</template>
+
+<style scoped lang="scss">
+.container-advantages-solo-heading {
+	align-items: flex-start;
+	text-align: left;
+
+	&__icon {
+		align-self: flex-start;
+	}
+
+	&__title {
+		color: var(--black);
+	}
+
+	&__cta {
+		color: var(--white);
+		background-color: var(--dark-blue);
+	}
+}
+</style>
