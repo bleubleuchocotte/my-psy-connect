a7a8e80b1d11cfbe816776172335cbb3d4a78c85
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-15 18:43:33 +0100

    feat: introduce basic structure

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,19 @@ useSeoMeta({
 </script>
 
 <template>
-	<main class="page-home" />
+	<main class="page-home">
+		<ContainerSolution />
+		<ContainerTeam />
+		<ContainerAdvantagesGroup />
+		<ContainerAdvantagesSolo />
+	</main>
 </template>
+
+<style scoped lang="scss">
+.page-home {
+	display: flex;
+	flex-direction: column;
+
+	@include gap(24);
+}
+</style>
