8cb7a4d9eee7b03bde2097fde78d856f22df7bfd
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-17 18:30:22 +0100

    chore: move into component

--- components/Container/Team/ContainerTeam.vue
+++ components/Container/Team/ContainerTeam.vue
@@ -1,25 +1,6 @@
 <template>
-	<UIContainer class="container-team">
-		<UIContainerHeading>
-			<template #icon>
-				<IconChat />
-			</template>
-			<template #title>
-				<h2>
-					Notre équipe de professionnels
-				</h2>
-			</template>
-			<template #text>
-				<p>
-					Chez MyPsy, nous réunissons des experts pour vous offrir un accompagnement personnalisé et de qualité, adapté à vos besoins.<br>Notre équipe vous assure un soutien professionnel en psychanalyse, psychothérapie et psychopraticie, avec une approche moderne et bienveillante.
-				</p>
-			</template>
-			<template #cta>
-				<UIButton class="container-team__cta">
-					Découvrir <IconArrowTopLeft />
-				</UIButton>
-			</template>
-		</UIContainerHeading>
+	<UIContainer>
+		<ContainerTeamHeading />
 		<UIContainerContent>
 			<template #content>
 				<ContainerTeamContent />
@@ -27,12 +8,3 @@
 		</UIContainerContent>
 	</UIContainer>
 </template>
-
-<style scoped lang="scss">
-.container-team {
-	&__cta {
-		color: var(--white);
-		background-color: var(--green);
-	}
-}
-</style>
---
+++ components/Container/Team/ContainerTeamHeading.vue
@@ -0,0 +1,31 @@
+<template>
+	<UIContainerHeading class="container-team-heading">
+		<template #icon>
+			<IconChat />
+		</template>
+		<template #title>
+			<h2>
+				Notre équipe de professionnels
+			</h2>
+		</template>
+		<template #text>
+			<p>
+				Chez MyPsy, nous réunissons des experts pour vous offrir un accompagnement personnalisé et de qualité, adapté à vos besoins.<br>Notre équipe vous assure un soutien professionnel en psychanalyse, psychothérapie et psychopraticie, avec une approche moderne et bienveillante.
+			</p>
+		</template>
+		<template #cta>
+			<UIButton class="container-team-heading__cta">
+				Découvrir <IconArrowTopLeft />
+			</UIButton>
+		</template>
+	</UIContainerHeading>
+</template>
+
+<style scoped lang="scss">
+.container-team-heading {
+	&__cta {
+		color: var(--white);
+		background-color: var(--green);
+	}
+}
+</style>
