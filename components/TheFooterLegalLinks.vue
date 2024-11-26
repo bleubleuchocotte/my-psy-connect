cda558a4b56f1b58e74b42a33a4ba9f7b4a82c4e
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-26 14:28:23 +0100

    feat: introduce footer

---
+++ components/TheFooterLegalLinks.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="footer-legal-links">
+		<UILink>
+			Politique de Confidentialité
+		</UILink>
+		<UILink>
+			Mentions légales
+		</UILink>
+		<UILink>
+			Politiques des cookies
+		</UILink>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.footer-legal-links {
+	display: flex;
+	align-items: center;
+	@include gap(20);
+}
+</style>
