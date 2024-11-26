cbf6622edecf48bb141bc45904d7e95d500b061b
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-11-26 14:54:30 +0100

    feat: remove unused icon

--- components/Icon/IconClose.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<g opacity="0.5">
-			<path d="M18.5 6L6.5 18" stroke="#0C1B33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-			<path d="M6.5 6L18.5 18" stroke="#0C1B33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-		</g>
-	</svg>
-</template>
