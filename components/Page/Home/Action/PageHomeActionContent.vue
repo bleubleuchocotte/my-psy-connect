0e87d59bbf9fe37e29115f3c810b3648f2eeebbe
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-10 16:08:58 +0100

    build: introduce action seciton

---
+++ components/Page/Home/Action/PageHomeActionContent.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import { LucideFileLock2, LucideHand, LucideMessageSquare, LucidePersonStanding, LucideSpeech } from "lucide-vue-next";
+</script>
+
+<template>
+	<UIContainerContent>
+		<template #title>
+			<h3>Ce que vous allez y trouver</h3>
+		</template>
+		<template #content>
+			<div class="page-home-action">
+				<UIBackgroundFixedSVG class="page-home-action__background" path="type-2" hide="mobile" />
+
+				<div class="page-home-action__cards">
+					<UIContainerContentCard :blur="true">
+						<template #icon>
+							<LucideSpeech stroke="var(--green)" stroke-width="2" />
+						</template>
+						<template #title>
+							<h3>
+								Renforcement de la confiance en soi
+							</h3>
+						</template>
+						<template #text>
+							<p>
+								Transformez vos peurs en force. Grâce aux échanges en groupe, développez votre capacité à vous exprimer librement et à oser prendre la parole, même en public, dans un environnement sécurisant.
+							</p>
+						</template>
+					</UIContainerContentCard>
+					<UIContainerContentCard :blur="true">
+						<template #icon>
+							<LucideHand stroke="var(--green)" stroke-width="2" />
+						</template>
+						<template #title>
+							<h3>
+								S'affranchir de la crainte de l'engagement
+							</h3>
+						</template>
+						<template #text>
+							<p>
+								Apprenez à dépasser votre peur de l'engagement, qu'il s'agisse d'une relation, d'un projet ou d'une étape de vie.<br>Rejoignez un groupe où vous pourrez partager vos appréhensions et découvrir des stratégies pour aller de l'avant.
+							</p>
+						</template>
+					</UIContainerContentCard>
+					<UIContainerContentCard :blur="true">
+						<template #icon>
+							<LucideFileLock2 stroke="var(--green)" stroke-width="2" />
+						</template>
+						<template #title>
+							<h3>
+								Anonymat garanti et confidentialité
+							</h3>
+						</template>
+						<template #text>
+							<p>
+								Tous nos groupes respectent strictement la confidentialité et l'anonymat, pour que vous puissiez vous exprimer sans retenue.<br>Ce cadre sécurisé favorise une ouverture authentique et libératrice.
+							</p>
+						</template>
+					</UIContainerContentCard>
+					<UIContainerContentCard :blur="true">
+						<template #icon>
+							<LucideMessageSquare stroke="var(--green)" stroke-width="2" />
+						</template>
+						<template #title>
+							<h3>
+								Dépasser le sentiment d'isolement
+							</h3>
+						</template>
+						<template #text>
+							<p>
+								Sentir que l'on n'est pas seul face à ses difficultés est essentiel. Nos groupes vous permettront de rencontrer des personnes qui partagent vos préoccupations, pour un soutien mutuel et des échanges constructifs.
+							</p>
+						</template>
+					</UIContainerContentCard>
+					<UIContainerContentCard :blur="true">
+						<template #icon>
+							<LucidePersonStanding stroke="var(--green)" stroke-width="2" />
+						</template>
+						<template #title>
+							<h3>
+								Retrouver un équilibre émotionnel
+							</h3>
+						</template>
+						<template #text>
+							<p>
+								Apprenez à gérer le stress, l'anxiété, et les émotions difficiles à travers des exercices concrets et des échanges encadrés, afin de retrouver une sérénité durable.
+							</p>
+						</template>
+					</UIContainerContentCard>
+				</div>
+			</div>
+		</template>
+	</UIContainerContent>
+</template>
+
+<style scoped lang="scss">
+.page-home-action {
+	&__cards {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+
+		@include gap(24);
+
+		& > div {
+			flex: 1;
+			@include prop("min-width", 315);
+		}
+
+		p {
+			text-wrap: balance;
+			@include font("p");
+		}
+	}
+
+	&__background {
+		transform: translateY(-25%);
+	}
+}
+</style>
---
+++ components/Page/Home/Action/PageHomeActionHeading.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { LucideMail } from "lucide-vue-next";
+</script>
+
+<template>
+	<UIContainerHeading class="page-home-action-heading">
+		<template #icon>
+			<IconCustomPath />
+		</template>
+		<template #title>
+			<h2 class="h1">
+				Passez à l'action, rejoignez<br>
+				un groupe de soutien MyPsy
+			</h2>
+		</template>
+		<template #text>
+			<p class="page-home-action-heading__text">
+				Participer à un groupe de soutien MyPsy, c'est l'opportunité de s'entourer de personnes bienveillantes qui traversent les mêmes défis que vous, avec l'accompagnement professionnel d'une psychologue expérimenté(e). Ensemble, avançons vers un avenir meilleur, en cultivant l'entraide, le partage et la coopération, des valeurs qui animent chacune de nos sessions.
+			</p>
+		</template>
+		<template #cta>
+			<UIButton class="page-home-action-heading__cta">
+				Rejoignez-nous <LucideMail stroke="var(--white)" stroke-width="2" />
+			</UIButton>
+		</template>
+	</UIContainerHeading>
+</template>
+
+<style scoped lang="scss">
+.page-home-action-heading {
+	&__cta {
+		background-color: var(--dark-blue);
+		color: var(--white);
+	}
+
+	&__text {
+		color: var(--fake-black);
+	}
+}
+</style>
