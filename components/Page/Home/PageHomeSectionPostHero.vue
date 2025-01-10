313f0de62cc603e98db988461d99a89fb7454519
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-10 13:16:52 +0100

    build: introduce post hero component

---
+++ components/Page/Home/PageHomeSectionPostHero.vue
@@ -0,0 +1,21 @@
+<template>
+	<UIContainer class="page-home-section-post-hero">
+		<UIBackgroundFixedSVG path="type-1" class="page-home-section-post-hero__background" />
+		<PageHomeSectionPostHeroHeading />
+		<PageHomeSectionPostHeroContent />
+	</UIContainer>
+</template>
+
+<style scoped lang="scss">
+.page-home-section-post-hero {
+	@media #{$desktop} {
+		gap: pxToRem(90) !important;
+	}
+
+	&__background {
+		@media not #{$desktop} {
+			display: none;
+		}
+	}
+}
+</style>
---
+++ components/Page/Home/PageHomeSectionPostHeroHeading.vue
@@ -0,0 +1,18 @@
+<template>
+	<UIContainerHeading class="page-home-section-hero-heading">
+		<template #icon>
+			<IconCustomCloud />
+		</template>
+		<template #title>
+			<h2>
+				Vous êtes en quête de solutions pour surmonter l'épuisement professionnel ?
+			</h2>
+		</template>
+	</UIContainerHeading>
+</template>
+
+<style scoped lang="scss">
+.page-home-section-hero-heading {
+	max-width: none;
+}
+</style>
