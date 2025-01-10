f289c495633a89bf9cc60baf457c94aed86e191a
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-10 12:13:46 +0100

    build: create hero structure

---
+++ components/Page/Home/PageHomeSectionHero.vue
@@ -0,0 +1,13 @@
+<template>
+	<UIContainer class="page-home-section-hero">
+		<PageHomeSectionHeroHeading />
+		<PageHomeSectionHeroContent />
+	</UIContainer>
+</template>
+
+<style scoped lang="scss">
+.page-home-section-hero {
+	padding-top: pxToRem(200);
+	background-color: var(--beige);
+}
+</style>
