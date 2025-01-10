60334ca2764094950fce9b4bc85f3cd3fcc08337
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-10 15:00:43 +0100

    build: introduce psychologist component

---
+++ components/Page/Home/Psychologist/PageHomePsychologistContent.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="page-home-psychologist-content">
+		<UIBackgroundFixedSVG path="type-1" class="page-home-psychologist-content__background" hide="mobile" />
+
+		<div class="page-home-psychologist-content__card">
+			<PageHomePsychologistContentCard>
+				<template #avatar>
+					<img src="/images/avatar/guenon.png" alt="">
+				</template>
+				<template #name>
+					<h3>
+						Isé Guénon
+					</h3>
+				</template>
+				<template #job>
+					<p>
+						Psychologue
+					</p>
+				</template>
+				<template #bio>
+					<p class="page-home-psychologist-content__card-bio">
+						Psychologue passionnée, reconnue pour son approche innovante et son engagement envers le bien-être mental.
+					</p>
+				</template>
+				<template #link>
+					en savoir plus
+				</template>
+			</PageHomePsychologistContentCard>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.page-home-psychologist-content {
+	&__background {
+		transform: translateY(-25%);
+	}
+
+	&__card {
+		position: relative;
+		display: flex;
+		justify-content: center;
+
+		@include prop("gap", 24);
+
+		& > div {
+			flex: 1;
+
+			@media #{$desktop} {
+				max-width: rem(260);
+			}
+		}
+
+		&-bio {
+			overflow: hidden;
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 4;
+			color: var(--fake-black);
+			@include font("p");
+		}
+	}
+}
+</style>
---
+++ components/Page/Home/Psychologist/PageHomePsychologistHeading.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { LucideArrowUpRight } from "lucide-vue-next";
+</script>
+
+<template>
+	<UIContainerHeading class="page-home-psychologist-heading">
+		<template #icon>
+			<IconCustomProfile />
+		</template>
+		<template #title>
+			<h2 class="h1">
+				Nos psychologues
+			</h2>
+		</template>
+		<template #text>
+			<p class="page-home-psychologist-heading__text">
+				Chez My psy, nous réunissons des psychologues, sensibles et formés<br>
+				à la souffrance au travail et au burn-out.
+			</p>
+		</template>
+		<template #cta>
+			<UIButton class="page-home-psychologist-heading__cta">
+				Découvrir <LucideArrowUpRight stroke="var(--white)" stroke-width="2" />
+			</UIButton>
+		</template>
+	</UIContainerHeading>
+</template>
+
+<style scoped lang="scss">
+.page-home-psychologist-heading {
+	&__text {
+		color: var(--fake-black);
+	}
+	&__cta {
+		color: var(--white);
+		background-color: var(--green);
+	}
+}
+</style>
