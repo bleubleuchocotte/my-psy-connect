db22387e1fc423aacd7d28f4f9b2a1f2f3c03ce9
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-13 07:46:14 +0100

    build: introduce mobile header

---
+++ components/Header/HeaderDesktop.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { LucideMail } from "lucide-vue-next";
+
+const { y } = useWindowScroll();
+</script>
+
+<template>
+	<header class="header">
+		<div class="header__absolute">
+			<IconLogoWithText class="header__absolute-logo" />
+
+			<UIButton class="header__absolute-cta">
+				Rejoignez-nous
+				<LucideMail stroke="var(--beige)" stroke-width="2" />
+			</UIButton>
+		</div>
+
+		<div class="header__navigation" :data-scroll="y > 10">
+			<TheNavigationBar />
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+	position: relative;
+
+	@include wrapper(1440px, 118);
+
+	&__absolute {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		position: absolute;
+		left: 0;
+		width: 100%;
+		@include prop("top", 24);
+		@include prop("padding-inline", 118);
+
+		&-cta {
+			background-color: var(--dark-blue);
+			color: var(--beige);
+		}
+	}
+
+	&__navigation {
+		position: fixed;
+		z-index: 999;
+
+		left: 50%;
+		transform: translateX(-50%);
+
+		transition: top 0.4s ease-out;
+
+		&[data-scroll="true"] {
+			--top: 24;
+		}
+		&[data-scroll="false"] {
+			--top: 48;
+		}
+
+		@include prop("top", var(--top));
+	}
+}
+</style>
---
+++ components/Header/Mobile/HeaderMobileMenuBurger.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { LucideMail, LucideX } from "lucide-vue-next";
+
+defineEmits<{
+	close: []
+}>();
+</script>
+
+<template>
+	<div class="header-mobile-menu-burger">
+		<div class="header-mobile-menu-burger__controls">
+			<IconLogoWithText class="header__absolute-logo" />
+			<UIButton @click="$emit('close')">
+				<LucideX stroke="var(--white)" stroke-width="2" />
+			</UIButton>
+		</div>
+
+		<nav class="header-mobile-menu-burger__navigation">
+			<NuxtLink class="h2 reset-ua" prefetch>
+				Accueil
+			</NuxtLink>
+			<NuxtLink class="h2 reset-ua" prefetch>
+				Service
+			</NuxtLink>
+			<NuxtLink class="h2 reset-ua" prefetch>
+				Partenaires
+			</NuxtLink>
+			<NuxtLink class="h2 reset-ua" prefetch>
+				Nos Psychologues
+			</NuxtLink>
+			<NuxtLink class="h2 reset-ua" prefetch>
+				Ressources
+			</NuxtLink>
+			<NuxtLink class="h2 reset-ua" prefetch>
+				À Propos
+			</NuxtLink>
+		</nav>
+
+		<UIButton class="header-mobile-menu-burger__contact-us">
+			Contactez-nous <LucideMail stroke="var(--beige)" stroke-width="2" />
+		</UIButton>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.header-mobile-menu-burger {
+	position: fixed;
+	left: 0;
+	top: 0;
+	z-index: 999;
+
+	width: 100vw;
+	height: 100vh;
+
+	padding: rem(20);
+
+	background-color: var(--beige);
+
+	&__controls {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		button {
+			justify-content: center;
+			height: 54px;
+			width: 54px;
+
+			padding: 0 !important;
+
+			background-color: var(--fake-black);
+		}
+	}
+
+	&__navigation {
+		display: flex;
+		flex-direction: column;
+
+		padding: rem(40);
+
+		a {
+			padding-block: rem(10);
+		}
+	}
+
+	&__contact-us {
+		width: 100%;
+		justify-content: center;
+		background-color: var(--dark-blue);
+		color: var(--beige);
+	}
+}
+</style>
