61df02a53aaae69ee4eeb6f2527194425ac2db0b
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-13 15:34:50 +0100

    build: introduce ressources page

---
+++ components/Page/Ressources/PageRessourcesColumns.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="page-ressources-columns">
+		<PageRessourcesColumnsStart class="page-ressources-columns__start" />
+		<PageRessourcesColumnsEnd class="page-ressources-columns__end" />
+	</section>
+</template>
+
+<style scoped lang="scss">
+.page-ressources-columns {
+	display: flex;
+	gap: rem(24);
+	padding-bottom: rem(94);
+
+	@media not #{$desktop} {
+		flex-direction: column;
+		gap: rem(20);
+	}
+
+	&__start,
+	&__end {
+		display: flex;
+		flex-direction: column;
+		gap: rem(24);
+		flex: 1;
+	}
+}
+</style>
--- pages/ressources.vue
+++ pages/ressources.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+import { LucideLightbulb, LucideNewspaper } from "lucide-vue-next";
+</script>
+
 <template>
-	<main />
+	<main class="page-ressources">
+		<PageHeader>
+			<template #title>
+				Ressources
+			</template>
+
+			<template #svg-background>
+				<IconCustomPath />
+			</template>
+
+			<template #icons>
+				<PageHeaderIcons>
+					<template #icon-1>
+						<LucideNewspaper stroke="var(--white)" stroke-width="2" />
+					</template>
+					<template #icon-2>
+						<LucideLightbulb stroke="var(--white)" stroke-width="2" />
+					</template>
+				</PageHeaderIcons>
+			</template>
+		</PageHeader>
+
+		<PageRessourcesColumns />
+
+		<ContainerFaq />
+	</main>
 </template>
+
+<style scoped lang="scss">
+.page-ressources {
+	display: flex;
+	flex-direction: column;
+	gap: rem(24);
+}
+</style>
