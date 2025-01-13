0bd5a61fc3f19178689fc383890cf194d3c23e9c
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-13 12:05:49 +0100

    build: introduce hero for partenaires page

--- pages/partenaires.vue
+++ pages/partenaires.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import { LucideHandshake, LucideSmilePlus } from "lucide-vue-next";
+</script>
+
 <template>
-	<main />
+	<main>
+		<PageHeader>
+			<template #title>
+				Partenaires
+			</template>
+
+			<template #svg-background>
+				<IconCustomStar />
+			</template>
+
+			<template #icons>
+				<PageHeaderIcons>
+					<template #icon-1>
+						<LucideSmilePlus stroke="var(--white)" stroke-width="2" />
+					</template>
+					<template #icon-2>
+						<LucideHandshake stroke="var(--white)" stroke-width="2" />
+					</template>
+				</PageHeaderIcons>
+			</template>
+
+			<template #description>
+				Voici une liste des organismes et institutions qui reconnaissent le potentiel de notre solution et nous accompagnent dans notre développement.<br>
+				Ces institutions de référence nous offrent un cadre solide pour développer avec efficacité cette plateforme de mise en relation entre professionnels et victimes de burnout.
+			</template>
+		</PageHeader>
+	</main>
 </template>
