28b776a55a46c262e7b89f9fad07031a4cc024d6
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-13 13:10:08 +0100

    chore: remove unused ocmponent

--- components/Icon/IconTextGreenCircleSprout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect width="82" height="82" rx="41" fill="#AAD5B8" />
-		<path d="M28.3667 61.3069H53.3667" stroke="white" stroke-width="4" stroke-linecap="square" stroke-linejoin="round" />
-		<path d="M35.8667 61.3069C49.6167 55.0569 37.8667 45.3069 43.3667 36.3069" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-		<path d="M34.6167 34.8069C37.3667 36.8069 39.1167 40.3069 40.3667 44.0569C35.3667 45.0569 31.6167 45.0569 28.3667 43.3069C25.3667 41.8069 22.6167 38.5569 20.8667 32.8069C27.8667 31.5569 31.8667 32.8069 34.6167 34.8069Z" stroke="white" stroke-width="4" stroke-linecap="square" stroke-linejoin="round" />
-		<path d="M46.1167 26.3069C44.2106 29.2858 43.2518 32.7723 43.3667 36.3069C48.1167 36.0569 51.6166 34.8069 54.1166 32.8069C56.6166 30.3069 58.1167 27.0569 58.3667 21.3069C51.6167 21.5569 48.3667 23.8069 46.1167 26.3069Z" stroke="white" stroke-width="4" stroke-linecap="square" stroke-linejoin="round" />
-	</svg>
-</template>
