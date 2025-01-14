182f93ed6eb328fa1c89c1ce718f27484baa0060
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-14 08:53:26 +0100

    build: introduce form page

---
+++ components/Page/Form/PageFormHero.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="container page-form-hero">
+		<div>TODO: form</div>
+		<img alt="" src="/images/about/french-tech-2.png">
+	</section>
+</template>
+
+<style scoped lang="scss">
+.page-form-hero {
+	display: flex;
+	justify-content: center;
+	gap: rem(24);
+	background-color: var(--beige);
+	padding-top: rem(171);
+
+	@media not #{$desktop} {
+		flex-direction: column;
+		gap: rem(40);
+		padding-top: rem(118);
+	}
+
+	img {
+		object-fit: cover;
+		border-radius: 10px;
+	}
+}
+</style>
---
+++ pages/formulaire.vue
@@ -0,0 +1,21 @@
+<template>
+	<main class="page page-formulaire">
+		<PageFormHero />
+		<PageHomePostHero class="page-formulaire__post-hero" />
+		<PageAboutTestimony2 class="page-formulaire__testimony" />
+		<ContainerFaq />
+	</main>
+</template>
+
+<style scoped lang="scss">
+.page-formulaire {
+	&__testimony {
+		background-color: var(--white);
+	}
+	&__post-hero {
+		@media not #{$desktop} {
+			padding-bottom: 0;
+		}
+	}
+}
+</style>
