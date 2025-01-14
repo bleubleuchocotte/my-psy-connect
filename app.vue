fd325bc92a60f6b8e2ba6b9d51f31882cb7447b3
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-14 18:20:45 +0100

    chore: update loading indicator color

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-	<NuxtLoadingIndicator :throttle="0" />
+	<NuxtLoadingIndicator :throttle="0" color="repeating-linear-gradient(90deg, var(--green) 0%, var(--dark-blue) 100%, var(--green) 200%)" />
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
