a736c757607b1bf1068b9960120da56d547eaffc
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-18 17:35:57 +0100

    build: introduce lenis

--- app.vue
+++ app.vue
@@ -59,8 +59,10 @@ useSeoMeta({
 </script>
 
 <template>
-	<NuxtLoadingIndicator :throttle="0" color="repeating-linear-gradient(90deg, var(--green) 0%, var(--dark-blue) 100%, var(--green) 200%)" />
-	<NuxtLayout>
-		<NuxtPage />
-	</NuxtLayout>
+	<UILenis>
+		<NuxtLoadingIndicator :throttle="0" color="repeating-linear-gradient(90deg, var(--green) 0%, var(--dark-blue) 100%, var(--green) 200%)" />
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</UILenis>
 </template>
