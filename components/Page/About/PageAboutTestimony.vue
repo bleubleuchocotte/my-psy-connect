42d227f9923c748d3b3e610e5fee66eac0e0376a
Thomas Auffroy <thomas.auffroy@gmail.com>
2025-01-18 16:48:30 +0100

    chore: update video testimony

--- components/Page/About/PageAboutTestimony.vue
+++ components/Page/About/PageAboutTestimony.vue
@@ -19,7 +19,7 @@
 		</UIContainerHeading>
 		<UIContainerContent class="page-about-testimony__content">
 			<template #content>
-				<iframe width="540" height="960" src="https://www.youtube.com/embed/KRntP-q_R9s?si=qpUBqHgN9RrAwXC1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
+				<iframe width="540" height="960" src="https://www.youtube.com/embed/2jrtuqgsCho?si=SRMpymS53kLEsNvC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
 			</template>
 		</UIContainerContent>
 	</UIContainer>
